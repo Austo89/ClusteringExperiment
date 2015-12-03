e8fdfce43b9f62da89cb31d2d0ca686b4b155f9e
fippy24 <connor.trumpet@gmail.com>
2015-12-03 12:21:51 -0700

    Stepping
    Stepping now works

--- src/clusteringexperiment/ACO.java
+++ src/clusteringexperiment/ACO.java
@@ -21,7 +21,7 @@ public class ACO {
 		grid = new double[gridSize][gridSize][2][dataLength];
 		antSize=in_ant;
 		maxStepSize=gridSize/2;//(int)Math.sqrt(20*(data.size()+in_ant));
-		System.out.println(gridSize+ " "+maxStepSize);
+		//System.out.println(gridSize+ " "+maxStepSize);
 	}
 	
 	public void cluster(){
@@ -31,7 +31,7 @@ public class ACO {
 		boolean notEnd = true;
 		for(int i=0; i<antSize; i++){
 			//pickup random object
-			ants.add(data.get(i));
+			//ants.add(data.get(i));
 			//System.arraycopy(data.get(i), 0, ants.get(i), 0, dataLength);
 			//get placed in random empty location
 			do {
@@ -44,7 +44,7 @@ public class ACO {
 			ants.add(new double[]{x, y});
 		}
 		scatterData(ants.size());
-		//printGrid();
+	
 		int iter=0;
 		int attempt=0;
 		while (notEnd&&iter<100){ //50000
@@ -66,13 +66,13 @@ public class ACO {
 	}
 	
 	private boolean step(int j){
-		int dir=(int)Math.random()*4;
+		int dir=(int)Math.floor(Math.random()*4);
 		int stepSize=(int)Math.floor(Math.random()*maxStepSize);
 		int x=(int)ants.get(j*2+1)[0];
 		int y=(int)ants.get(j*2+1)[1];
-		System.out.print(y);
+		//System.out.print(ants.size());
 		if (dir==0){
-			x%=gridSize;
+			x=x%gridSize;
 		}else if (dir==1){
 			if (stepSize>x){
 				x=gridSize-(stepSize-x);
@@ -87,9 +87,9 @@ public class ACO {
 			}
 		}else{
 			y+=stepSize;
-			y%=gridSize;
+			y=y%gridSize;
 		}
-		System.out.println("x,y,j, step: "+x+", "+y+", "+j+", "+stepSize);
+		//System.out.println("x,y,j, step, dir, sizes: "+x+", "+y+", "+j+", "+stepSize+", "+dir+", "+ants.get(j*2).length+" "+grid[x][y][1].length);
 		if (grid[x][y][1][0]==0){
 			System.arraycopy(ants.get(j*2), 0, grid[x][y][1], 0, dataLength);
 			Arrays.fill(grid[(int)ants.get(j*2+1)[0]] [(int)ants.get(j*2+1)[1]] [1], 0);
